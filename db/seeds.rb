fc1b0c5567517b453ab46eb67d4580e061bcb5bc
Pete Hanner <petehanner@gmail.com>
2019-10-17 14:51:18 -0400

    copy seed data to feed to front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-weapons = Weapon.new(
+weapons = Weapon.create(
   [
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Lance',
+      base_dmg: 20,
+      atk_mod: 60,
+      def_mod: -20,
+      img: 'img.com/imgsrc'
+    },
     {
       name: 'Lance',
       base_dmg: 20,
@@ -18,8 +74,56 @@ weapons = Weapon.new(
   ]
 )
 
-armors = Armor.new(
+armors = Armor.create(
   [
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
+    {
+      name: 'Chain Mail',
+      base_def: 60,
+      spd_mod: -0.1,
+      img: 'img.com/imgsrc'
+    },
     {
       name: 'Chain Mail',
       base_def: 60,
@@ -29,10 +133,66 @@ armors = Armor.new(
   ]
 )
 
-horses = Horse.new(
+horses = Horse.create(
   [
     {
       name: 'Peyton',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Marisa',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'German',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Pete',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Carlos',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Victor',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Rob',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'JC',
+      atk_mod: 0,
+      def_mod: 20,
+      spd_mod: -0.2,
+      img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/11/14/shetland-foal-pony.jpg?w968h681'
+    },
+    {
+      name: 'Shannon',
       atk_mod: 0,
       def_mod: 20,
       spd_mod: -0.2,
